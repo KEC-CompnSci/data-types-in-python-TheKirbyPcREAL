610900f4e675cce38e89fab31879d3d3971cbab8
TheKirbyPcREAL <27lemusa@retsdstudent.ca>
2025-02-19 20:59:44 +0000

    5

--- src/assignment.py
+++ src/assignment.py
@@ -5,7 +5,13 @@
 num_str_1 = "42"
 num_str_2 = "13"
 num_str_3 = "7"
-word_1 = "robots"
-word_2 = "built"
-word_3 = "today"
-word_4 = "were"
+word_1 = " robots"
+word_2 = " built"
+word_3 = " today"
+word_4 = " were"
+
+real_number = int(num_str_1) + int(num_str_2) 
+real_num_string = int(real_number)
+num = str(real_num_string)
+sentence = num + (word_1) + (word_4) + (word_2) +  (word_3)
+print (sentence)
